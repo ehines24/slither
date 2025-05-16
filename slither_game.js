4f4c07eb51378c452b37d0c6e779eaab5db79e5c
Eddie <ehines@umass.edu>
2025-05-16 14:00:57 -0400

    Added a game grid to the canvas

--- slither_game.js
+++ slither_game.js
@@ -1,8 +1,24 @@
-const create_canvas = () => {
+const create_canvas = (width, height) => function() {
     const canvas = document.getElementById("game");
-    const ctx = canvas.getContext("2d");
-    console.log("Loaded canvas.");
-    ctx.fillStyle = "green";
-    ctx.fillRect(0,0,640,480);
+    if(canvas.getContext) {
+        const ctx = canvas.getContext("2d");
+        ctx.fillRect(0,0,width,height);
+        draw_grid(ctx, width, height);
+    }
 };
-window.onload = create_canvas;
+const draw_grid = (ctx, width, height, divFactor = 10) => {
+    ctx.strokeStyle = "#FFFFFF";
+    console.log("here");
+    let grid = new Path2D();
+    console.log("Here too!");
+    for(let i = 10; i < width; i+=divFactor) {
+       grid.moveTo(i,0);
+       grid.lineTo(i,height);
+    }
+    for(let j = 10; j < height; j+=divFactor) {
+       grid.moveTo(0,j);
+       grid.lineTo(width,j);
+    }
+    ctx.stroke(grid);
+};
+window.onload = create_canvas(640,480);
