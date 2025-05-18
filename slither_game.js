46e6bb3a37a9fdc9bc89d39fd9738b508e44d17a
Eddie <ehines@umass.edu>
2025-05-18 11:14:17 -0400

    Gave the game a header bar and cleaned up the look of the title block.

--- slither_game.js
+++ slither_game.js
@@ -7,25 +7,27 @@ const create_canvas = (width, height) => function() {
         ctx.shadowOffsetX = 2;
         ctx.shadowOffsetY = 2;
         ctx.shadowBlur = 2;
-        ctx.fillStyle = "purple";
+        ctx.fillStyle = "green";
         ctx.shadowColor = "rgb(0 0 0 / 50%)";
         ctx.font = "48px sans-serif";
         ctx.fillText("Slither", 250, 48);
     }
 };
-const draw_grid = (ctx, width, height, divFactor = 10) => {
+const draw_grid = (ctx, width, height, divFactor = 10, startingHeight=60) => {
     // CSS colors are supported here
     // Stroke styles can be used with format strings to programmatically change throughout a for loop
     // Or you can use the CanvasGradient object
     ctx.strokeStyle = "#FFFFFF";
+    ctx.fillStyle = "#AAA";
     console.log("here");
+    ctx.fillRect(0,0, width, startingHeight);
     let grid = new Path2D();
     console.log("Here too!");
-    for(let i = 10; i < width; i+=divFactor) {
-       grid.moveTo(i+.5,0);
+    for(let i = 0; i < width; i+=divFactor) {
+       grid.moveTo(i+.5, startingHeight);
        grid.lineTo(i+.5,height);
     }
-    for(let j = 10; j < height; j+=divFactor) {
+    for(let j = startingHeight; j < height; j+=divFactor) {
        grid.moveTo(0,j+.5);
        grid.lineTo(width,j+.5);
     }
